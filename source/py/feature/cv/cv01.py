2fb9304f3150302fac7d59b96948155d738f81d8
manjaoe <36906174+manjaoe@users.noreply.github.com>
2025-09-04 15:34:10 +0800

    Update cv01.py

--- source/py/feature/cv/cv01.py
+++ source/py/feature/cv/cv01.py
@@ -7,60 +7,60 @@ sfx = ".cv01"
 
 def cv01_subst():
     return [
-        ast.subst_map(
-            "$",
-            target_suffix=sfx,
-        ),
-        ast.subst_map(
-            "%",
-            target_suffix=sfx,
-        ),
-        ast.subst_map(
-            ["&", "&&", "&&&"],
-            target_suffix=sfx,
-        ),
-        ast.subst_map(
-            ["@", "~@"],
-            target_suffix=sfx,
-        ),
+        # ast.subst_map(
+        #     "$",
+        #     target_suffix=sfx,
+        # ),
+        # ast.subst_map(
+        #     "%",
+        #     target_suffix=sfx,
+        # ),
+        # ast.subst_map(
+        #     ["&", "&&", "&&&"],
+        #     target_suffix=sfx,
+        # ),
+        # ast.subst_map(
+        #     ["@", "~@"],
+        #     target_suffix=sfx,
+        # ),
         ast.subst_map(
             ["Q", "Q.bg"],
             target_suffix=sfx,
         ),
-        ast.subst_map(
-            [
-                "<=<",
-                ">=>",
-                "<!--",
-                "<#--",
-                "xml_empty_comment.liga",  # <!---->
-                *infinite_helper.ignore_when_enabled(
-                    "=>",
-                    "<==",
-                    "==>",
-                    "<=>",
-                    "<==>",
-                    "<=|",
-                    "|=>",
-                    "<-|",
-                    "|->",
-                    "<-",
-                    "->",
-                    "<--",
-                    "-->",
-                    "<-<",
-                    ">->",
-                    "<->",
-                ),
-                *infinite_helper.ignore_when_disabled(
-                    ast.gly_seq("<=", "sta"),
-                    ast.gly_seq(">=", "end"),
-                    ast.gly_seq("<-", "sta"),
-                    ast.gly_seq(">-", "end"),
-                ),
-            ],
-            target_suffix=sfx,
-        ),
+        # ast.subst_map(
+        #     [
+        #         "<=<",
+        #         ">=>",
+        #         "<!--",
+        #         "<#--",
+        #         "xml_empty_comment.liga",  # <!---->
+        #         *infinite_helper.ignore_when_enabled(
+        #             "=>",
+        #             "<==",
+        #             "==>",
+        #             "<=>",
+        #             "<==>",
+        #             "<=|",
+        #             "|=>",
+        #             "<-|",
+        #             "|->",
+        #             "<-",
+        #             "->",
+        #             "<--",
+        #             "-->",
+        #             "<-<",
+        #             ">->",
+        #             "<->",
+        #         ),
+        #         *infinite_helper.ignore_when_disabled(
+        #             ast.gly_seq("<=", "sta"),
+        #             ast.gly_seq(">=", "end"),
+        #             ast.gly_seq("<-", "sta"),
+        #             ast.gly_seq(">-", "end"),
+        #         ),
+        #     ],
+        #     target_suffix=sfx,
+        # ),
     ]
 
 
